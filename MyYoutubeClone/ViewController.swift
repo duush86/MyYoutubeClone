44d340b704d148d7923dfd736e3e6e46b4cb3ce6
Antonio <j.antonio.oro@gmail.com>
2019-11-28 13:32:19 -0600

    Initial Commit

---
+++ MyYoutubeClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyYoutubeClone
+//
+//  Created by Antonio Orozco on 11/28/19.
+//  Copyright © 2019 Antonio Orozco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
